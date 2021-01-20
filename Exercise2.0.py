b5b9b802373903106eeeeab44bdfcfaa373935ba
ssero85 <77705236+ssero85@users.noreply.github.com>
2021-01-19 21:34:04 -0500

    Update Exercise2.0.py

--- Exercise2.0.py
+++ Exercise2.0.py
@@ -1,4 +1,4 @@
 print("Shanda Sero")
 print("1/19/2021")
 print("At what size of a project should you start using version management software? Why?")
-print("I believe that the size of the project does not matter when using a version management software. The software allows you save all changes, work out any bugs, allow other users to access your work, and much more. The software offers many features like allowing you to go back to previous versions of what you're working on. Many people can work on the same project at once, and they can also comment on any line and other users can converse with them for clarification or whatever may be needed.
+print("I believe that the size of the project does not matter when using a version management software. The software allows you save all changes, work out any bugs, allow other users to access your work, and much more. The software offers many features like allowing you to go back to previous versions of what you're working on. Many people can work on the same project at once, and they can also comment on any line and other users can converse with them for clarification or whatever may be needed.")
